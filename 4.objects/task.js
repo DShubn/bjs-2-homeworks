301abdbd9dc15f6aa254d9c73d6f82883a01a65a
Dshubn <dima120896@gmail.com>
2024-12-11 23:17:31 +0300

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,9 +1,12 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
