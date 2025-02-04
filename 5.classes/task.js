7df2c4ede4c33d47d10bc654f5bb062dfbff4e49
Dshubn <dima120896@gmail.com>
2025-02-04 23:19:01 +0300

    change task.js 2

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -24,37 +24,37 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-    constructor() {
-        super()
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
         this.type = "magazine"
     }
 }
 
 class Book extends PrintEditionItem {
-    constructor(author) {
-        super()
-        this.author = author;
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount)
+        this.author = author
         this.type = "book"
     }
 }
 
 class NovelBook extends Book {
-  constructor() {
-    super()
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author)
     this.type = "novel"
   }
 }
 
 class FantasticBook extends Book {
-  constructor() {
-    super()
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author)
     this.type = "fantastic"
   } 
 }
 
 class DetectiveBook extends Book {
-  constructor() {
-    super()
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author)
     this.type = "detective"
   }
 }
